da7b332582d4f070eddfc178633fd247aec57033
Joshua Huang <jphuangjr@gmail.com>
2016-02-23 19:37:19 -0800

    Pushed examples

--- Algorithms/intro.js
+++ Algorithms/intro.js
@@ -1,8 +1,25 @@
 // Arrays
+	var list = [1,2,"string", function(){}];
+			   //0,1, 2			,3
+			   var target = list[2]
 
 // JavaScript Object / Hash Tables
+	var hash = {
+		key: "value",
+		key2: 1
+	}
+	var target = hash["key"]
+	var alsoTarget = hash.key
 
 // JavaScript Function
+	var myfunct = function(){
+
+	}
+
+	function myfunction(){
+		
+	}
+
 
 //
 

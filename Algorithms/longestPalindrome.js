ef9c1c224bebdf79d5b44f632975a83d5bc14cb4
Joshua Huang <jphuangjr@gmail.com>
2016-02-25 22:01:35 -0800

    added longestPalindrome, nthFib, and reduce.js

---
+++ Algorithms/longestPalindrome.js
@@ -0,0 +1,11 @@
+//A palindrome is a word, phrase, number, or other sequence of
+// characters which reads the same backward or forward. 
+//Ex. aha, deified. 
+
+//Given a string, find the longest palindrone in the string. 
+//eg. "he stressed desserts." "stressed desserts" is the longest palidrome
+//Extra Credit: Try to do this recursively
+
+var longestPalindrome = function(string){
+
+}
---
+++ Algorithms/nthFibonacci.js
@@ -0,0 +1,13 @@
+//The Fibonacci Sequence is the series of numbers: 
+//0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ... 
+//The next number is found by adding up the two numbers before it.
+
+
+//Make a function that returns a array of the sequence of the nthFibonnaci number
+//Ex. nthFibonnaci(5) => [0,1,1,2,3]
+
+//**Hint**: Use Recursion
+
+var nthFibonacci = function (n) {
+  // TODO: implement me!
+};

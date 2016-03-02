b103251578237220ce7dc229c92e43d65cd62089
Joshua Huang <jphuangjr@gmail.com>
2016-03-01 23:56:13 -0800

    added bubble sort

---
+++ Algorithms/bubbleSort.js
@@ -0,0 +1,25 @@
+
+
+// Bubble sort  works by starting at the first element of an array and
+// comparing it to the second element; if the first element is greater than the
+// second element, it swaps the two. It then compares the second to the third,
+// and the third to the fourth, and so on; in this way, the largest values
+// "bubble" to the end of the array. Once it gets to the end of the array, it
+// starts over and repeats the process until the array is sorted numerically.
+
+//HERE is a GIF of bubble sort: https://commons.wikimedia.org/wiki/File:Bubble-sort-example-300px.gif
+
+//Time Complexity:
+
+// Extra credit: Optimize your solution so that during any given pass, if no elements are
+// swapped we can assume the list is sorted and can exit the function early.
+// After this optimization, what is the time complexity of your algorithm?
+//
+// More Extra Credit: Do you need to consider every element every time you iterate
+// through the array?  Again: Has the time complexity of your algorithm changed?
+
+
+var bubbleSort = function(array) {
+	// Your code here.
+
+};

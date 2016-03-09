1c8299bd2689b23b0e4daba6832bd4071b5c91f0
Joshua Huang <jphuangjr@gmail.com>
2016-03-08 23:19:47 -0800

    firstnonrepeated charater

---
+++ Algorithms/firstNonRepeatedCharacter.js
@@ -0,0 +1,9 @@
+  //Given an arbitrary input string, return the first nonrepeated character in
+  //the string. For example:
+  //
+  //  firstNonRepeatedCharacter('ABA'); // => 'B'
+  //  firstNonRepeatedCharacter('AACBDB'); // => 'C'
+
+  var firstNonRepeatedCharacter = function(string) {
+	  // TODO: your code here
+  };

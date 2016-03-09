411f0be168f4b75673ed1a4600055429ddf3920a
Joshua Huang <jphuangjr@gmail.com>
2016-03-08 23:09:05 -0800

    added balancedParens.js

---
+++ Algorithms/balancedParens.js
@@ -0,0 +1,9 @@
+//write a function that takes a string of text and returns true if the parentheses are balanced and false otherwise.
+//
+// Example:
+//   balancedParens('(');  // false
+//   balancedParens('()'); // true
+//   balancedParens('(]'); // false
+//   balancedParens(')(');  // false
+//   balancedParens('([)]');  // false
+//   balancedParens('([])');  // true

982ff8133a37db977197942aed80be9e080555de
Joshua Huang <jphuangjr@gmail.com>
2016-03-08 23:14:39 -0800

    added subsetOf.js

---
+++ Algorithms/subsetOf.js
@@ -0,0 +1,7 @@
+//Create an Array method that will return whether or not the array is a subset of the input array.
+//Assume the values in the array only hold Strings and Ints
+
+Array.prototype.isSubsetOf = function (arr) {
+	//TODO: Insert Code Here
+
+}
